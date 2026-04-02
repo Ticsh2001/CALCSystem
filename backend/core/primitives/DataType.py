cd872d14e6280a0c8740c7020a9d64be522870ca
victor tishchenko <ticsh2001@yandex.ru>
2026-04-02 17:05:17 +0300

    ddd

--- backend/core/primitives/DataType.py
+++ backend/core/primitives/DataType.py
@@ -1,11 +1,11 @@
 from enum import Enum, auto
 from typing import Union, Any, Optional, Type, TypeVar
+from numbers import Number
 import numpy as np
 
 T = TypeVar("T", bound="EnumClassAbstraction")
 
 class EnumClassAbstraction:
-
     """Миксин, реализующий универсальный from_input для Enum‑классов."""
     @classmethod
     def from_input(cls: Type[T], value: Union[str, T]) -> T:
@@ -26,7 +26,7 @@ class EnumClassAbstraction:
 # ---------------------------------------------------------------------------
 
 class ValueStatus(Enum, EnumClassAbstraction):
-    '''Статусы значений для отслеживания состояния параметра'''
+    """Статусы значений для отслеживания состояния параметра"""
     UNKNOWN   = auto()  # Величина неизвестна
     DEPEND    = auto()  # Значение пришло от связи с другим элементом
     CALCULATED= auto()  # Значение было рассчитано в этом элементе
@@ -35,7 +35,7 @@ class ValueStatus(Enum, EnumClassAbstraction):
 # ---------------------------------------------------------------------------
 
 class DataType(Enum, EnumClassAbstraction):
-    '''Типы данных, которыми обмениваются элементы'''
+    """Типы данных, которыми обмениваются элементы"""
     FLOAT       = auto()  # С плавающей точкой
     LOGIC       = auto()  # Логические
     LOGIC_TRUE  = auto()  # ИСТИНА – конкретный результат логического выражения (для ветвления)
@@ -46,15 +46,61 @@ class DataType(Enum, EnumClassAbstraction):
 
         
 class ValueSpec:
-    '''Спецификаия величины, метаинформация о величине/параметре'''
+    """Спецификаия величины, метаинформация о величине/параметре"""
     def __init__(self, value_name: str = '', dimension: str = ''):
         self.value_name = value_name  # Название величины (давление/температура)
         self.dimension = dimension  # Размерность величины
     
+def validate_data(data: Any, dtype: DataType,
+                  min_val: Optional[Union[int, float]]=None,
+                  max_val: Optional[Union[int, float]]=None):
+
+    # Вспомогательные функции для проверки типов
+    def _is_numeric(value: Any):
+        """Являются ли данные с типом с плавающей запятой"""
+        if isinstance(value, (Number, bool)):
+            return True
+        if isinstance(value, (list, tuple)):
+            return all(_is_numeric(v) for v in value)
+        if isinstance(value, np.ndarray):
+            return np.issubdtype(value.dtype, np.number)
+        return False
+
+    def _is_integer(value):
+        """Являются ли данные целочисленными"""
+        if isinstance(value, (int, bool)):
+            return True
+        if isinstance(value, (list, tuple)):
+            return all(_is_integer(v) for v in value)
+        if isinstance(value, np.ndarray):
+            return np.issubdtype(value.dtype, np.integer) or np.issubdtype(value.dtype, np.bool_)
+        return False
+
+    def _is_logical(value):
+        """Являются ли данные логическими"""
+        # Логические: bool, 0/1
+        if isinstance(value, bool):
+            return True
+        if isinstance(value, int) and value in (0, 1):
+            return True
+        if isinstance(value, (list, tuple)):
+            return all(_is_logical(v) for v in value)
+        if isinstance(value, np.ndarray):
+            # если массив bool или целочисленный с 0/1
+            if np.issubdtype(value.dtype, np.bool_):
+                return True
+            if np.issubdtype(value.dtype, np.integer):
+                return np.all((value == 0) | (value == 1))
+        return False
+
     
+
+
+
+
         
 class Value:
-    ''' Описание параметра
+    """ Описание параметра
     name - имя параметра
     value - значение параметра
     value_spec - спецификация параметра
@@ -64,7 +110,7 @@ class Value:
     store_prev - сохранять ли предыдущее состояние
     min_value - минимальное значение
     max_value - максимальное значение
-    '''
+    """
     def __init__(self, name: str, 
                  value: Any, 
                  value_spec: ValueSpec,
@@ -74,6 +120,7 @@ class Value:
                  store_prev: bool = False,
                  min_value: Optional[Any] = None,
                  max_value: Optional[Any] = None):
+
         self._name = name
         self._description = description
         self._status = status
